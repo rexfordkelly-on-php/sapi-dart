f803cb7879371a74e3a5d343b0b0b61e1697805f
Rex R Kelly <rexfordkelly@gmail.com>
2018-12-02 01:09:37 -0500

    initial commit

---
+++ views/TemplateException.php
@@ -0,0 +1,11 @@
+<?php
+namespace Dart\View;
+
+/**
+ * View Exception
+ *
+ * @package Dart
+ * @author Vance Lucas <vance@vancelucas.com>
+ * @license http://www.opensource.org/licenses/bsd-license.php
+ */
+class TemplateException extends \Exception {}
---
+++ views/TemplateMissingException.php
@@ -0,0 +1,12 @@
+<?php
+namespace Dart\View\Exception;
+use Dart\View\Exception;
+
+/**
+ * View Template Missing Exception
+ *
+ * @package Dart
+ * @author Vance Lucas <vance@vancelucas.com>
+ * @license http://www.opensource.org/licenses/bsd-license.php
+ */
+class TemplateMissingException extends Exception {}
